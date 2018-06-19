f958b6f9e93480e42c9f7a5c5f099422cedf3689
maxi <maxinuss@gmail.com>
2018-06-19 15:00:07 -0300

    Payment logic

--- src/Infraestructure/Application/Payment/Payment.php
+++ src/Infraestructure/Application/Payment/Payment.php
@@ -1,7 +1,23 @@
 <?php
-namespace App;
+declare(strict_types=1);
+
+namespace App\Infraestructure\Application\Payment;
+
+use App\Domain\Model\RentalType\RentalType;
 
 class Payment
 {
+    /**
+     * @param float $amount
+     */
+    public function make(RentalType $rentalType)
+    {
+        $finalPrice = $rentalType->getPrice() - $rentalType->getDiscount();
+
+        /**
+         * Payment logic here. Not developed in this test project.
+         */
+    }
+
 
 }
